b983f1909eb2a01ece04cb9a8b55fe38e8f03023
Jinjun Liang <kumkee@users.noreply.github.com>
2023-07-15 13:20:36 +1200

    added ssh softlink; updated ssh

--- softlinks.sh
+++ softlinks.sh
@@ -2,3 +2,4 @@ ln -s $PWD/home-manager/profile ~/.profile
 rm -ivrf $PWD/nvim/lua/custom
 ln -s $PWD/nvim_custom $PWD/nvim/lua/custom
 ln -s $PWS/nvim_custom/configs/ftplugin $PWD/nvim/.
+ln -s $PWD/ssh $HOME/.ssh
