58fc0b5ba0b79e5f5b0e697ccb417c43d1746251
Jinjun Liang <kumkee@users.noreply.github.com>
2023-07-15 22:53:35 +1200

    a few soft links achieved in home.nix

--- softlinks.sh
+++ softlinks.sh
@@ -1,5 +1,4 @@
-ln -s $PWD/home-manager/profile ~/.profile
-rm -ivrf $PWD/nvim/lua/custom
-ln -s $PWD/nvim_custom $PWD/nvim/lua/custom
-ln -s $PWS/nvim_custom/configs/ftplugin $PWD/nvim/.
+# rm -ivrf $PWD/nvim/lua/custom
+# ln -s $PWD/nvim_custom $PWD/nvim/lua/custom
+# ln -s $PWD/nvim_custom/configs/ftplugin $PWD/nvim/.
 ln -s $PWD/ssh $HOME/.ssh
